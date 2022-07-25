f4ae762eccc6751583d8357c643d7701d78ed4ff
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-07-24 20:56:47 -0500

    Added entitlements and a script (`entitlements.sh`) to codesign the `snapUtil` binary after running `make`.
    To use `entitlements.sh`, put your developer ID into a new file named `config.sh` by putting `developerID='putYourDeveloperIDHere'` (it is of the form `Apple Development: yourEmailHere (yourIDNumberOfSomeSortHere)` and can be gotten from the keychain access app on macOS)

---
+++ entitlements.sh
@@ -0,0 +1,13 @@
+source config.sh # Put your developer ID in here using `developerID='putYourDeveloperIDHere'` (it is of the form `Apple Development: yourEmailHere (yourIDNumberOfSomeSortHere)` and can be gotten from the keychain access app on macOS)
+
+security find-identity -v -p codesigning
+# Use this to put into the "-s" arg here:
+codesign --force -s "$developerID" --entitlements snapUtil.entitlements snapUtil
+# Now sudo ./snapUtil works!
+# Disable SIP too: `csrutil disable` and `nvram boot-args= amfi_get_out_of_my_way=0x1`... maybe..: https://github.com/ahl/apfs/issues/2 : {"
+# I managed to run it on Catalina with these two steps:
+# 
+# I added the com.apple.developer.vfs.snapshot entitlement when signing the binary. Even ad-hoc signing works.
+# I rebooted to recovery and disabled SIP and AMFI with the commands csrutil disable and nvram boot-args= amfi_get_out_of_my_way=0x1.
+# After booting back to macOS proper, I can run snapUtil and create and manage snapshots. You can then reenable security with csrutil enable and nvram -d boot-args.
+# "}
